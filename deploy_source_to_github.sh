51c37dd30979011aea691c1a4e324d65de0d90ff
chengjun <wangchj04@gmail.com>
2019-10-14 13:50:45 +0800

    rebuilding site 2019年10月14日 星期一 13时50分45秒 CST

--- deploy_source_to_github.sh
+++ deploy_source_to_github.sh
@@ -2,7 +2,7 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-ssh-add ~/.ssh/id_rsa
+#ssh-add ~/.ssh/id_rsa
 git remote set-url origin git@github.com:ccr-china/ccr-china.git
 #git checkout gh-pages
 
